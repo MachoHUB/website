c19e3a801e573b6f932f0c31717c39f2c6c1bf96
emir macho <emirayse272727@gmail.com>
2026-08-06 01:26:09 +0300

    fix tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
