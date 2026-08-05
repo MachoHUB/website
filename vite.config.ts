79411e99837b403617e57858db6affdc32b19151
emir macho <emirayse272727@gmail.com>
2026-08-06 01:21:57 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Özel domain (loggercreate.xyz) kullandığı için base '/'
   base: '/',
   plugins: [
     react(),
-    tailwindcss(),
   ],
   resolve: {
     alias: {
